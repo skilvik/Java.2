b0275571a763f914a825dcf1689033d310e75c7e
Lidia <skilvik@gmail.com>
2020-12-11 12:32:42 +0300

    ДЗ4

---
+++ lesson4/sample/Controller.java
@@ -0,0 +1,21 @@
+package lesson4.sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    public void send() {
+        textArea.appendText(textField.getText()+"\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+
+
+}
